511e0c94514bfe80031e88b44645390f7cb83826
JamesVanWaza <jgacuca@gmail.com>
2020-12-19 23:43:00 -0500

    Added the webpack-config file

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require("path");
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const webpack = require("webpack"); // to access built-in plugins
+
+module.exports = {
+    mode: 'development',
+    entry: './src/js/index.js',
+    output: {
+        path: path.resolve(__dirname, "public"),
+        filename: "[name].js",
+    },
+    devServer: {
+        open: 'Google Chrome',
+        contentBase: path.join(__dirname, "public"),
+        port: 9010
+    },
+    module: {
+        rules: [{
+                test: /\.txt$/,
+                use: 'raw-loader'
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // Creates `style` nodes from JS Strings
+                    { loader: 'style-loader' },
+
+                    // Translates CSS into CommonJS
+                    { loader: 'css-loader' },
+
+                    // Compiles Sass to CSS
+                    { loader: 'sass-loader' }
+                ]
+            },
+            // Start here for the URL Loader
+            {
+                test: /\.(png|jpg)$/,
+                use: [
+                    { loader: 'url-loader' }
+                ]
+            },
+            {
+                test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9]\.png|jpg)?$/,
+                use: 'url-loader?limit=10000',
+            },
+            {
+                test: /\.(ttf|eot|svg)(\?[\s\S]+)?$/,
+                use: 'file-loader',
+            }
+        ]
+    }
+};
