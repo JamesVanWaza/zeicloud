fcd93ff8affa0f2a7a6fead5f1be277d5c9c3770
JamesVanWaza <jgacuca@gmail.com>
2021-02-22 08:43:43 -0500

    WIP still trying to setup forms

---
+++ src/js/signup.js
@@ -0,0 +1,11 @@
+/** 1 State the form variables */
+let fname = document.querySelector('#fname').value;
+// let lname = document.querySelector('#lname').value;
+let signupbtn = document.querySelector('#signup-btn');
+
+/** 2 Validate user fields */
+
+/** 3 Prevent Button submission */
+// signupbtn.addEventListener("click", e => {
+//     e.preventDefault();
+// });// still a wip
