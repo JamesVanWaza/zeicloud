689ab6695da23e452b14964992d91b96ab28be6c
JamesVanWaza <jgacuca@gmail.com>
2023-09-01 15:53:16 -0400

    Uncommented out the firestore read, which in return messes with firestore create

--- src/js/app.js
+++ src/js/app.js
@@ -224,56 +224,56 @@ if (zeSignOut) {
 }
 
 /** Firestore Read Template */
-// var stdNo = 0;
+var stdNo = 0;
 
-// var tbody = document.getElementById('tbody1');
+var tbody = document.getElementById('tbody1');
 
-// function AddItemToTable(name, roll, sec, gen) {
-//     let trow = document.createElement("tr");
-//     let td1 = document.createElement('td');
-//     let td2 = document.createElement('td');
-//     let td3 = document.createElement('td');
-//     let td4 = document.createElement('td');
-//     let td5 = document.createElement('td');
+function AddItemToTable(name, roll, sec, gen) {
+    let trow = document.createElement("tr");
+    let td1 = document.createElement('td');
+    let td2 = document.createElement('td');
+    let td3 = document.createElement('td');
+    let td4 = document.createElement('td');
+    let td5 = document.createElement('td');
 
-//     td1.innerHTML = ++stdNo;
-//     td2.innerHTML = name;
-//     td3.innerHTML = roll;
-//     td4.innerHTML = sec;
-//     td5.innerHTML = gen;
+    td1.innerHTML = ++stdNo;
+    td2.innerHTML = name;
+    td3.innerHTML = roll;
+    td4.innerHTML = sec;
+    td5.innerHTML = gen;
 
-//     trow.appendChild(td1);
-//     trow.appendChild(td2);
-//     trow.appendChild(td3);
-//     trow.appendChild(td4);
-//     trow.appendChild(td5);
+    trow.appendChild(td1);
+    trow.appendChild(td2);
+    trow.appendChild(td3);
+    trow.appendChild(td4);
+    trow.appendChild(td5);
 
-//     tbody.appendChild(trow);
-// }
+    tbody.appendChild(trow);
+}
 
-// function AddAllItemsToTable(TheStudent) {
-//     stdNo = 0;
-//     tbody.innerHTML = "";
-//     TheStudent.forEach(element => {
-//         AddItemToTable(element.NameOfStd, element.RollNo, element.Section, element.Gender);
-//     });
-// }
+function AddAllItemsToTable(TheStudent) {
+    stdNo = 0;
+    tbody.innerHTML = "";
+    TheStudent.forEach(element => {
+        AddItemToTable(element.NameOfStd, element.RollNo, element.Section, element.Gender);
+    });
+}
 
-// window.onload = function () {
-//     GetAllDataOnce();
+window.onload = function () {
+    GetAllDataOnce();
 
-//     async function GetAllDataOnce() {
-//         const querySnapshot1 = await getDocs(collection(db, 'TheStudentsList'));
+    async function GetAllDataOnce() {
+        const querySnapshot1 = await getDocs(collection(db, 'zeStudentsListForm'));
 
-//         var students = [];
+        var students = [];
 
-//         querySnapshot1.forEach(doc => {
-//             students.push(doc.data());
+        querySnapshot1.forEach(doc => {
+            students.push(doc.data());
 
-//             AddAllItemsToTable(students);
-//         });
-//     }
-// }
+            AddAllItemsToTable(students);
+        });
+    }
+}
 
 // window.onload = function () {
 //     GetAllDataRealTime();
